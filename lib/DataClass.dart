784ef9792998cd509c2a7187b6c80a4f9e4cfb3e
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-27 09:16:14 +0500

    YA ALLAH

--- lib/DataClass.dart
+++ lib/DataClass.dart
@@ -1,13 +1,16 @@
+
 import 'dart:convert';
 import 'package:flutter_lab_5/product_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-class DataClass extends ChangeNotifier {
+class DataClass extends ChangeNotifier
+{
   List<Products>? post;
   bool loading = false;
 
-  getData() async {
+  getData() async
+  {
     loading = true;
     post = (await _fetchingData());
     loading = false;
@@ -15,14 +18,21 @@ class DataClass extends ChangeNotifier {
   }
 }
 
-Future<List<Products>> _fetchingData() async {
+Future<List<Products>> _fetchingData() async
+{
   final respnse = await http.get(Uri.parse('https://dummyjson.com/products'));
-  if (respnse.statusCode == 200) {
+  if (respnse.statusCode == 200)
+  {
     List<dynamic> _parsedList = jsonDecode(respnse.body)['products'];
-    List<Products> _itemsList = List<Products>.from(
-        _parsedList.map<Products>((dynamic i) => Products.fromJson(i)));
+    List<Products> _itemsList = List<Products>.from
+    (
+      _parsedList.map<Products>((dynamic i) => Products.fromJson(i))
+    );
     return _itemsList;
-  } else {
+  }
+  else
+  {
     throw Exception('Erorrr');
   }
 }
+
